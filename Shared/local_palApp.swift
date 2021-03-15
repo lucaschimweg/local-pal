4ad51423f80e926a28c2107bac5d6318cd0600df
janbraunsdorff <jan99braunsdorff@web.de>
2021-03-15 19:00:30 +0100

    Initial Commit

---
+++ Shared/local_palApp.swift
@@ -0,0 +1,17 @@
+//
+//  local_palApp.swift
+//  Shared
+//
+//  Created by Schimweg, Luca on 15/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct local_palApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
